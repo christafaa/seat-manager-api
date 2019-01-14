90dc9a245b420098145d1e5d40c089c0440deebb
Chris Alberti <christopherjalberti@gmail.com>
2019-01-14 00:10:53 -0500

    setup production environment

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SeatManagerApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
